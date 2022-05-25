0f6190361355235a0e4b88e14adfbf34644880a3
Edi Mihai <iancumihai19@stud.ase.ro>
2022-05-25 19:53:58 +0300

    lab07 added

---
+++ lab07/src/CarWithHeating.java
@@ -0,0 +1,25 @@
+
+
+public class CarWithHeating extends CarAbstractDecorator {
+
+    private int numberOfSeats;
+
+    public CarWithHeating(Vehicle vehicle) {
+        super(vehicle);
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    @Override
+    public void startEngine() {
+        vehicle.startEngine();
+        System.out.println("Heating seats...");
+    }
+
+}
